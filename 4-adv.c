cef419e49d65f614ee137be602577546d255af90
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-06-22 19:37:33 +0100

    Update 4-adv.c

--- 4-adv.c
+++ 4-adv.c
@@ -8,17 +8,12 @@
  */
 void _sub(stack_t **stack, unsigned int line_n)
 {
-	stack_t *nm;
-	int sub;
-
-	nm = *stack;
-	if (!nm || nm->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%d: can't sub, stack too short\n", line_n);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_n);
 		exit(EXIT_FAILURE);
 	}
-	sub = nm->next->n - nm->n;
-	nm->next->n = sub;
+	(*stack)->next->n -= (*stack)->n;
 	_pop(stack, line_n);
 }
 /**
