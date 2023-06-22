0af13ac8702f03edb3a0cae1459a5dbc72c4021e
CANDELY001 <125865959+CANDELY001@users.noreply.github.com>
2023-06-22 17:19:43 +0100

    Update useful.c

--- useful.c
+++ useful.c
@@ -1,5 +1,25 @@
 #include "monty.h"
+/**
+ * _getline - a function google it
+ * @line: parameter
+ * @line_length: parameter
+ * @file: file atrik l9anb
+ * Return: WEEKWEEK wa bghit anghawt
+ */
+ssize_t _getline(char **line, size_t *line_length, FILE *file)
+{
+	ssize_t read_bytes = getline(line, line_length, file);
 
+	if (read_bytes == -1)
+	{
+		if (feof(file))
+			clearerr(file);
+		else
+			perror("Error: getline failed");
+	}
+
+	return (read_bytes);
+}
 /**
  * _isdigit - function that checks for a digit
  * @c: parameter
